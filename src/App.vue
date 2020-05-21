30ae7d4a4fd07dac819acfa236ea6aaeac5bfe55
Julian Lagerberg <jl@jlagerberg.se>
2020-05-21 12:42:43 +0200

    Started on first toggle

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <span role="checkbox" tabindex="0" aria-checked="false" class="first">
+      <span aria-hidden="true" class="second" :class="{ hoger: test }"></span>
+    </span>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    test: false,
+  }),
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  min-height: 100vh;
+  background: #2c3e50;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  height: 100vh;
+}
+
+.first {
+  width: 50px;
+  height: 25px;
+  background: grey;
+  position: relative;
+  display: inline-block;
+  flex-shrink: 0;
+  border: 2px solid black;
+  border-radius: 6px;
+}
+
+.second {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #fff;
+}
+
+.hoger {
+  transform: translateX(25px);
 }
 </style>
