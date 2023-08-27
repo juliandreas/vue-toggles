728c15312fec3f39183e5e967b24a4905a22d102
Julian Lagerberg <juliandreas@fastmail.com>
2023-08-27 13:35:31 +0200

    fix packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,33 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": "warn",
+    "no-debugger": "warn",
+    "no-unused-vars": "warn",
+    "vue/script-setup-uses-vars": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "object-shorthand": "error",
+    "prefer-template": "error",
+    "no-loop-func": "error",
+    "default-param-last": "error",
+    "no-param-reassign": "error",
+    "prefer-spread": "error",
+    "prefer-arrow-callback": "error",
+    "no-duplicate-imports": "error",
+    "dot-notation": "error",
+    "no-unneeded-ternary": "error",
+    "prefer-destructuring": "warn",
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,7 @@
-// import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "VueToggles",
-      fileName: "vue-toggles"
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue"
-        }
-      }
-    }
-  }
+  plugins: [vue()]
 });
