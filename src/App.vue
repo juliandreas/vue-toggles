7b11f205c45c2d9272ca322fdd67d09e6fd772a3
Julian Lagerberg <juliandreas@fastmail.com>
2023-08-27 19:03:12 +0200

    remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-// import Test from "./components/VueToggles.vue";
-import { VueToggles } from "vue-toggles";
+import VueToggles from "./components/VueToggles.vue";
 
 const checked = ref(false);
 </script>
