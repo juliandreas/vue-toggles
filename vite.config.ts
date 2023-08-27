860a863d8882f0335d0a18706886c1981d9cb6d4
Julian Lagerberg <juliandreas@fastmail.com>
2023-08-27 14:15:56 +0200

    update configs and package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  publicDir: false,
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "VueToggles",
+      fileName: "vue-toggles",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
