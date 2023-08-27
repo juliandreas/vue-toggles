17caeb541bf6ced428dabd5be6ca4c401fce74a6
Julian Lagerberg <juliandreas@fastmail.com>
2023-08-27 10:26:46 +0200

    add tests

--- src/__tests__/VueToggles.spec.ts
+++ src/__tests__/VueToggles.spec.ts
@@ -2,16 +2,14 @@ import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import VueToggles from '../components/VueToggles.vue';
 
-describe('VueToggles', () => {
-  it('renders correctly', () => {
-    const wrapper = mount(VueToggles, {
-      props: {
-        value: false
-      }
-    });
-    expect(wrapper.element).toMatchSnapshot();
-  });
+const hexToRgb = (hex: string) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+    : null;
+};
 
+describe('VueToggles', () => {
   it('toggles when clicked', async () => {
     const wrapper = mount(VueToggles, {
       props: {
@@ -29,6 +27,62 @@ describe('VueToggles', () => {
       }
     });
     await wrapper.trigger('click');
-    expect(wrapper.emitted()).not.toHaveProperty('toggle-state');
+    expect(wrapper.emitted().click).toBeUndefined();
+  });
+
+  it('handles width prop correctly', () => {
+    const width = 100;
+    const wrapper = mount(VueToggles, {
+      props: {
+        value: false,
+        width
+      }
+    });
+    expect(wrapper.attributes('style')).toContain(`width: ${width}px;`);
+  });
+
+  it('handles height prop correctly', () => {
+    const height = 30;
+    const wrapper = mount(VueToggles, {
+      props: {
+        value: false,
+        height
+      }
+    });
+    expect(wrapper.attributes('style')).toContain(`height: ${height}px;`);
+  });
+
+  it('displays checked text correctly', () => {
+    const checkedText = 'On';
+    const wrapper = mount(VueToggles, {
+      props: {
+        value: true,
+        checkedText
+      }
+    });
+    expect(wrapper.text()).toBe(checkedText);
+  });
+
+  it('displays unchecked text correctly', () => {
+    const uncheckedText = 'Off';
+    const wrapper = mount(VueToggles, {
+      props: {
+        value: false,
+        uncheckedText
+      }
+    });
+    expect(wrapper.text()).toBe(uncheckedText);
+  });
+
+  it('applies checked background color', async () => {
+    const checkedBg = '#123456';
+    const wrapper = mount(VueToggles, {
+      props: {
+        value: true,
+        checkedBg
+      }
+    });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.attributes('style')).toContain(`background: ${hexToRgb(checkedBg)};`);
   });
 });
