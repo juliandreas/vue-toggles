70ba7a727ba4fe884c45c3cccd3ec467cbbedb87
Julian Lagerberg <juliandreas@fastmail.com>
2023-08-27 14:32:34 +0200

    test component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import VueToggles from "./components/VueToggles.vue";
+// import VueToggles from "./components/VueToggles.vue";
+import { VueToggles } from "vue-toggles";
 
 const checked = ref(false);
 </script>
